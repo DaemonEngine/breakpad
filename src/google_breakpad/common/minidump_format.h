0cacdcd9f3ae8a4f0f853552eced1aa4e582c9d1
Zaid Elkurdi <elkurdi@google.com>
2023-01-25 15:56:36 -0800

    Add support for reading annotation objects in Crashpad modules
    At the moment, the Minidump class only supports reading simple and
    list annotations from Crashpad minidumps. This change adds support
    for reading annotation objects stored in Crashpad modules
    (MDRawModuleCrashpadInfo) and exposes them via a new getter in
    MinidumpCrashpadInfo.
    
    Change-Id: I033fc4a4fdff5901babc2472e0150f79af56b830
    Reviewed-on: https://chromium-review.googlesource.com/c/breakpad/breakpad/+/4195756
    Reviewed-by: Joshua Peraza <jperaza@chromium.org>

--- src/google_breakpad/common/minidump_format.h
+++ src/google_breakpad/common/minidump_format.h
@@ -1097,9 +1097,22 @@ typedef struct {
 } MDRawSimpleStringDictionary;
 
 typedef struct {
+  MDRVA name;
+  uint16_t type;
+  uint16_t reserved;
+  MDRVA value;
+} MDRawCrashpadAnnotation;
+
+typedef struct {
+  uint32_t count;
+  MDLocationDescriptor objects[0]; /* MDRawCrashpadAnnotation */
+} MDRawCrashpadAnnotationList;
+
+typedef struct {
   uint32_t version;
   MDLocationDescriptor list_annotations;
   MDLocationDescriptor simple_annotations;  /* MDRawSimpleStringDictionary */
+  MDLocationDescriptor annotation_objects;  /* MDRawCrashpadAnnotationList */
 } MDRawModuleCrashpadInfo;
 
 typedef struct {
