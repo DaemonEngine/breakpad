98c1614dc6d786fc4ff79ee52d7fe1c310291427
Brian Sheedy <bsheedy@chromium.org>
2023-02-16 12:35:22 -0800

    Fix sprintf usage
    Changes a recent introduction of sprintf to snprintf since sprintf is
    deprecated in Chromium.
    
    Bug: crashpad:329
    Change-Id: Icd346da4c86bd8e867266dfebaf617991dd90113
    Reviewed-on: https://chromium-review.googlesource.com/c/breakpad/breakpad/+/4261633
    Reviewed-by: Joshua Peraza <jperaza@chromium.org>

--- src/processor/minidump.cc
+++ src/processor/minidump.cc
@@ -5540,7 +5540,7 @@ void MinidumpCrashpadInfo::Print() {
         // Value represents something else.
         char buffer[3];
         for (const uint8_t& v : annot.value) {
-          sprintf(buffer, "%X", v);
+          snprintf(buffer, sizeof(buffer), "%X", v);
           str_value.append(buffer);
         }
       }
