effd88c0b414d2eaee45f5a402d90747c4645edd
Tom Burgin <bur@google.com>
2024-07-25 16:32:30 -0400

    upload_system_symbols: break into go packages
    This will make reusing this code easier. Since there are multiple
    packages now, the build needs updating. A separate CL in Chromium will
    follow this change.
    
    Change-Id: I8d95d13ea61d32684d0ef845c3d1da2cd6d4cd7f
    Reviewed-on: https://chromium-review.googlesource.com/c/breakpad/breakpad/+/5742415
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>

--- src/tools/mac/upload_system_symbols/arch/arch_reader.go
+++ src/tools/mac/upload_system_symbols/arch/arch_reader.go
@@ -27,7 +27,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-package main
+package arch
 
 import (
 	"debug/macho"
@@ -38,10 +38,10 @@ import (
 */
 import "C"
 
-// getArchStringFromHeader takes a MachO FileHeader and returns a string that
+// GetArchStringFromHeader takes a MachO FileHeader and returns a string that
 // represents the CPU type and subtype.
 // This function is a Go version of src/common/mac/arch_utilities.cc:BreakpadGetArchInfoFromCpuType().
-func getArchStringFromHeader(header macho.FileHeader) string {
+func GetArchStringFromHeader(header macho.FileHeader) string {
 	// TODO(rsesek): As of 10.9.4, OS X doesn't list these in /usr/include/mach/machine.h.
 	if header.Cpu == C.kCPU_TYPE_ARM64 && header.SubCpu == C.kCPU_SUBTYPE_ARM64_ALL {
 		return "arm64"
--- src/tools/mac/upload_system_symbols/archive/extract.go
+++ src/tools/mac/upload_system_symbols/archive/extract.go
@@ -1,4 +1,4 @@
-package main
+package archive
 
 // #cgo LDFLAGS: -lParallelCompression
 // #include <stdio.h>
