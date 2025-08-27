37bf6b17e2ccd95984c071646d27cc8b227ec4bb
Ben Hamilton <benhamilton@google.com>
2025-08-27 15:53:18 -0600

    [DWARF] Skip past vendor-defined content descriptions
    From the DWARF 5 standard § 6.2.4.2:
    
    > 6.2.4.2 Vendor-defined Content Descriptions
    >
    > Vendor-defined content descriptions may be defined using content
    > type codes in the range DW_LNCT_lo_user to DW_LNCT_hi_user. Each
    > such code may be combined with one or more forms from the set:
    > DW_FORM_block, DW_FORM_block1, DW_FORM_block2, DW_FORM_block4,
    > DW_FORM_data1, DW_FORM_data2, DW_FORM_data4, DW_FORM_data8,
    > DW_FORM_data16, DW_FORM_flag, DW_FORM_line_strp, DW_FORM_sdata,
    > DW_FORM_sec_offset, DW_FORM_string, DW_FORM_strp, DW_FORM_strx,
    > DW_FORM_strx1, DW_FORM_strx2, DW_FORM_strx3, DW_FORM_strx4 and
    > DW_FORM_udata.
    >
    > If a consumer encounters a vendor-defined content type that it does
    > not understand, it should skip the content data as though it were
    > not present.
    
    This updates Breakpad's DWARF 5 LineInfo parser to skip past
    vendor-defined content types that it does not understand.
    
    Bug: http://b/441328287
    Change-Id: I23056be4dacc4585419a37d4b8c83270da4ab4cf
    Reviewed-on: https://chromium-review.googlesource.com/c/breakpad/breakpad/+/6886571
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- src/common/dwarf/dwarf2reader.cc
+++ src/common/dwarf/dwarf2reader.cc
@@ -60,6 +60,103 @@
 
 namespace google_breakpad {
 
+namespace {
+
+const uint8_t* SkipFormAttribute(ByteReader* reader, uint16_t version,
+                                 const uint8_t* start, uint32_t form) {
+  size_t len;
+
+  switch (form) {
+    case DW_FORM_indirect:
+      form =
+          static_cast<enum DwarfForm>(reader->ReadUnsignedLEB128(start, &len));
+      start += len;
+      return SkipFormAttribute(reader, version, start, form);
+
+    case DW_FORM_flag_present:
+    case DW_FORM_implicit_const:
+      return start;
+    case DW_FORM_addrx1:
+    case DW_FORM_data1:
+    case DW_FORM_flag:
+    case DW_FORM_ref1:
+    case DW_FORM_strx1:
+      return start + 1;
+    case DW_FORM_addrx2:
+    case DW_FORM_ref2:
+    case DW_FORM_data2:
+    case DW_FORM_strx2:
+      return start + 2;
+    case DW_FORM_addrx3:
+    case DW_FORM_strx3:
+      return start + 3;
+    case DW_FORM_addrx4:
+    case DW_FORM_ref4:
+    case DW_FORM_data4:
+    case DW_FORM_strx4:
+    case DW_FORM_ref_sup4:
+      return start + 4;
+    case DW_FORM_ref8:
+    case DW_FORM_data8:
+    case DW_FORM_ref_sig8:
+    case DW_FORM_ref_sup8:
+      return start + 8;
+    case DW_FORM_data16:
+      return start + 16;
+    case DW_FORM_string:
+      // TODO(b/441382965): Pass in the length of the attribute and use memchr()
+      // instead of strlen().
+      return start + strlen(reinterpret_cast<const char*>(start)) + 1;
+    case DW_FORM_udata:
+    case DW_FORM_ref_udata:
+    case DW_FORM_strx:
+    case DW_FORM_GNU_str_index:
+    case DW_FORM_GNU_addr_index:
+    case DW_FORM_addrx:
+    case DW_FORM_rnglistx:
+    case DW_FORM_loclistx:
+      reader->ReadUnsignedLEB128(start, &len);
+      return start + len;
+
+    case DW_FORM_sdata:
+      reader->ReadSignedLEB128(start, &len);
+      return start + len;
+    case DW_FORM_addr:
+      return start + reader->AddressSize();
+    case DW_FORM_ref_addr:
+      // DWARF2 and 3/4 differ on whether ref_addr is address size or
+      // offset size.
+      assert(version >= 2);
+      if (version == 2) {
+        return start + reader->AddressSize();
+      } else if (version >= 3) {
+        return start + reader->OffsetSize();
+      }
+      break;
+
+    case DW_FORM_block1:
+      return start + 1 + reader->ReadOneByte(start);
+    case DW_FORM_block2:
+      return start + 2 + reader->ReadTwoBytes(start);
+    case DW_FORM_block4:
+      return start + 4 + reader->ReadFourBytes(start);
+    case DW_FORM_block:
+    case DW_FORM_exprloc: {
+      uint64_t size = reader->ReadUnsignedLEB128(start, &len);
+      return start + size + len;
+    }
+    case DW_FORM_strp:
+    case DW_FORM_line_strp:
+    case DW_FORM_strp_sup:
+    case DW_FORM_sec_offset:
+      return start + reader->OffsetSize();
+  }
+  fprintf(stderr, "Unhandled form type 0x%x\n", form);
+  return nullptr;
+}
+
+}  // namespace
+
 const SectionMap::const_iterator GetSectionByName(const SectionMap&
                                                   sections, const char *name) {
   assert(name[0] == '.');
@@ -207,93 +304,7 @@ const uint8_t* CompilationUnit::SkipDIE(const uint8_t* start,
 // Skips a single attribute form's data.
 const uint8_t* CompilationUnit::SkipAttribute(const uint8_t* start,
                                               enum DwarfForm form) {
-  size_t len;
-
-  switch (form) {
-    case DW_FORM_indirect:
-      form = static_cast<enum DwarfForm>(reader_->ReadUnsignedLEB128(start,
-                                                                     &len));
-      start += len;
-      return SkipAttribute(start, form);
-
-    case DW_FORM_flag_present:
-    case DW_FORM_implicit_const:
-      return start;
-    case DW_FORM_addrx1:
-    case DW_FORM_data1:
-    case DW_FORM_flag:
-    case DW_FORM_ref1:
-    case DW_FORM_strx1:
-      return start + 1;
-    case DW_FORM_addrx2:
-    case DW_FORM_ref2:
-    case DW_FORM_data2:
-    case DW_FORM_strx2:
-      return start + 2;
-    case DW_FORM_addrx3:
-    case DW_FORM_strx3:
-      return start + 3;
-    case DW_FORM_addrx4:
-    case DW_FORM_ref4:
-    case DW_FORM_data4:
-    case DW_FORM_strx4:
-    case DW_FORM_ref_sup4:
-      return start + 4;
-    case DW_FORM_ref8:
-    case DW_FORM_data8:
-    case DW_FORM_ref_sig8:
-    case DW_FORM_ref_sup8:
-      return start + 8;
-    case DW_FORM_data16:
-      return start + 16;
-    case DW_FORM_string:
-      return start + strlen(reinterpret_cast<const char*>(start)) + 1;
-    case DW_FORM_udata:
-    case DW_FORM_ref_udata:
-    case DW_FORM_strx:
-    case DW_FORM_GNU_str_index:
-    case DW_FORM_GNU_addr_index:
-    case DW_FORM_addrx:
-    case DW_FORM_rnglistx:
-    case DW_FORM_loclistx:
-      reader_->ReadUnsignedLEB128(start, &len);
-      return start + len;
-
-    case DW_FORM_sdata:
-      reader_->ReadSignedLEB128(start, &len);
-      return start + len;
-    case DW_FORM_addr:
-      return start + reader_->AddressSize();
-    case DW_FORM_ref_addr:
-      // DWARF2 and 3/4 differ on whether ref_addr is address size or
-      // offset size.
-      assert(header_.version >= 2);
-      if (header_.version == 2) {
-        return start + reader_->AddressSize();
-      } else if (header_.version >= 3) {
-        return start + reader_->OffsetSize();
-      }
-      break;
-
-    case DW_FORM_block1:
-      return start + 1 + reader_->ReadOneByte(start);
-    case DW_FORM_block2:
-      return start + 2 + reader_->ReadTwoBytes(start);
-    case DW_FORM_block4:
-      return start + 4 + reader_->ReadFourBytes(start);
-    case DW_FORM_block:
-    case DW_FORM_exprloc: {
-      uint64_t size = reader_->ReadUnsignedLEB128(start, &len);
-      return start + size + len;
-    }
-    case DW_FORM_strp:
-    case DW_FORM_line_strp:
-    case DW_FORM_strp_sup:
-    case DW_FORM_sec_offset:
-      return start + reader_->OffsetSize();
-  }
-  fprintf(stderr,"Unhandled form type 0x%x\n", form);
-  return nullptr;
+  return SkipFormAttribute(reader_, header_.version, start, form);
 }
 
 // Read the abbreviation offset from a compilation unit header.
@@ -544,6 +555,8 @@ const uint8_t* CompilationUnit::ProcessOffsetBaseAttribute(
       return start + 16;
     case DW_FORM_string: {
       const char* str = reinterpret_cast<const char*>(start);
+      // TODO(b/441382965): Pass in the length of the attribute and use memchr()
+      // instead of strlen().
       return start + strlen(str) + 1;
     }
     case DW_FORM_udata:
@@ -709,6 +722,8 @@ const uint8_t* CompilationUnit::ProcessAttribute(
     case DW_FORM_string: {
       const char* str = reinterpret_cast<const char*>(start);
       ProcessAttributeString(dieoffset, attr, form, str);
+      // TODO(b/441382965): Pass in the length of the attribute and use memchr()
+      // instead of strlen().
       return start + strlen(str) + 1;
     }
     case DW_FORM_udata:
@@ -1378,6 +1393,8 @@ const char* LineInfo::ReadStringForm(uint32_t form, const uint8_t** lineptr) {
   const char* name = nullptr;
   if (form == DW_FORM_string) {
     name = reinterpret_cast<const char*>(*lineptr);
+    // TODO(b/441382965): Pass in the length of the attribute and use memchr()
+    // instead of strlen().
     *lineptr += strlen(name) + 1;
     return name;
   } else if (form == DW_FORM_strp) {
@@ -1465,8 +1482,37 @@ void LineInfo::ReadFileRow(const uint8_t** lineptr,
         *lineptr += 16;
         break;
       default:
-        fprintf(stderr, "Unrecognized form in line table header. %d\n",
-                content_types[col]);
+        if (content_types[col] >= DW_LNCT_lo_user &&
+            content_types[col] <= DW_LNCT_hi_user) {
+          // Vendor-defined content descriptions may be defined using content
+          // type codes in the range DW_LNCT_lo_user to DW_LNCT_hi_user.
+          //
+          // Each such code may be combined with one or more forms from the set:
+          // DW_FORM_block, DW_FORM_block1, DW_FORM_block2, DW_FORM_block4,
+          // DW_FORM_data1, DW_FORM_data2, DW_FORM_data4, DW_FORM_data8,
+          // DW_FORM_data16, DW_FORM_flag, DW_FORM_line_strp, DW_FORM_sdata,
+          // DW_FORM_sec_offset, DW_FORM_string, DW_FORM_strp, DW_FORM_strx,
+          // DW_FORM_strx1, DW_FORM_strx2, DW_FORM_strx3, DW_FORM_strx4 and
+          // DW_FORM_udata.
+          const uint8_t* new_lineptr = SkipFormAttribute(
+              reader_, header_.version, *lineptr, content_forms[col]);
+          if (new_lineptr == nullptr) {
+            fprintf(stderr,
+                    "Unable to skip attribute, content_type=0x%04x, "
+                    "content_form=0x%04x\n",
+                    content_types[col], content_forms[col]);
+            // TODO(b/441383538): Return an error here instead of asserting.
+            assert(false);
+            break;
+          }
+          *lineptr = new_lineptr;
+          break;
+        }
+        fprintf(stderr,
+                "Unrecognized form in line table header: content_type=0x%04x, "
+                "content_form=0x%04x\n",
+                content_types[col], content_forms[col]);
+        // TODO(b/441383538): Return an error here instead of asserting.
         assert(false);
         break;
     }
@@ -1551,6 +1597,8 @@ void LineInfo::ReadHeader() {
       while (*lineptr) {
         const char* dirname = reinterpret_cast<const char*>(lineptr);
         handler_->DefineDir(dirname, dirindex);
+        // TODO(b/441382965): Pass in the length of the attribute and use memchr()
+        // instead of strlen().
         lineptr += strlen(dirname) + 1;
         dirindex++;
       }
@@ -1768,6 +1816,8 @@ bool LineInfo::ProcessOneOpcode(ByteReader* reader,
         case DW_LNE_define_file: {
           const char* filename = reinterpret_cast<const char*>(start);
 
+          // TODO(b/441382965): Pass in the length of the attribute and use memchr()
+          // instead of strlen().
           templen = strlen(filename) + 1;
           start += templen;
 
