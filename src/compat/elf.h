6e6f1ca999eed3e681849ce357aace1f71289b7c
Thomas Debesse <dev@illwieckz.net>
2026-07-31 06:01:06 +0200

    Revert "old-elf"
    This reverts commit 58a72dabfa6cf23967f8bee0d72cf6b9271092be.

--- src/compat/elf.h
+++ src/compat/elf.h
@@ -7,20 +7,4 @@
 #include <elf.h>
 #endif
 
-#ifndef SHF_COMPRESSED
-#define SHF_COMPRESSED 0x800
-#endif
-
-#ifndef ELFCOMPRESS_ZLIB
-#define ELFCOMPRESS_ZLIB 1
-#endif
-
-#ifndef EM_RISCV
-#define EM_RISCV 243
-#endif
-
-#ifndef EM_NDS32
-#define EM_NDS32 167
-#endif
-
 #endif // SRC_COMPAT_ELF_H_
